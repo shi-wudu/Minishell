1d495566c707883501cfc3376d14d414b445a466
Marta Figueiredo <97525594+shi-wudu@users.noreply.github.com>
2025-08-13 21:34:01 +0100

    First built in

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marleand <marleand@student42.lisboa.pt>    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-08-13 15:15:54 by marleand          #+#    #+#             */
+/*   Updated: 2025-08-13 15:15:54 by marleand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+# include "Libft/libft.h"
+# include "ft_printf/ft_printf.h" 
