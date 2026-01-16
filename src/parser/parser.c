8b622c69d515f00d78a417775512195dcea5476e
SergioHawky <sergioilkiv14@gmail.com>
2026-01-16 16:15:16 +0000

    norminette

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,11 +6,23 @@
 /*   By: seilkiv <seilkiv@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:18:58 by seilkiv           #+#    #+#             */
-/*   Updated: 2026/01/16 14:06:18 by seilkiv          ###   ########.fr       */
+/*   Updated: 2026/01/16 16:10:28 by seilkiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	parser_handle_token(t_cmd **cmd, t_cmd **head,
+			t_token **tk, t_data *data)
+{
+	if ((*tk)->type == WORD || (*tk)->type == PIPE)
+		return (handle_word_pipe(cmd, head, tk));
+	if ((*tk)->type == INPUT || (*tk)->type == TRUNC
+		|| (*tk)->type == APPEND || (*tk)->type == HEREDOC)
+		return (parse_redirect_token(*cmd, tk, data));
+	*tk = (*tk)->next;
+	return (true);
+}
 
 // Cria e inicializa um novo comando.
 // Define-o como cabeça da lista caso ainda não exista.
@@ -28,17 +40,8 @@ t_cmd	*parser(t_token *tokens, t_data *data)
 	{
 		if (!cmd)
 			cmd = new_cmd(&head, NULL);
-		if (tk->type == WORD || tk->type == PIPE)
-			if (!handle_word_pipe(&cmd, &head, &tk))
-				return (NULL);
-		else if (tk->type == INPUT || tk->type == TRUNC
-			|| tk->type == APPEND || tk->type == HEREDOC)
-		{
-			if (!parse_redirect_token(cmd, &tk, data))
-				return (NULL);
-		}
-		else
-			tk = tk->next;
+		if (!parser_handle_token(&cmd, &head, &tk, data))
+			return (NULL);
 	}
 	return (head);
 }
